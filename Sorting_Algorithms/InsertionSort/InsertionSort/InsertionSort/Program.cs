12903c520d5fcf4687710b9e055da40125a134ee
Lenovo <Lenovo@DAVIDCOMPUTER>
2016-11-18 09:32:58 +0200

    Sorts

---
+++ Sorting_Algorithms/InsertionSort/InsertionSort/InsertionSort/Program.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InsertionSort
+{
+    class Program
+    {
+       public static Stopwatch sw = Stopwatch.StartNew();
+        public static void Main(string[] args) {
+            int[] A = new int[100000];
+
+          
+           
+            //stopwatch.Stop();
+            //Console.WriteLine(stopwatch.ElapsedMilliseconds);
+
+
+            populateArray(A);
+            Console.WriteLine("Berfore sorting : ");
+            PrintArray(A);
+            // sort the array
+            Console.WriteLine();
+            insertionSort(A);
+            Console.WriteLine();
+            Console.WriteLine("After Sorting");
+
+            PrintArray(A);
+
+            sw.Stop();
+
+            Console.WriteLine("Time taken: {0}s", sw.Elapsed.TotalSeconds);
+
+        }
+
+        /**
+         * This method will sort the integer array using insertion sort in C#
+         * algorithm
+         * 
+         * 
+         */
+        private static void insertionSort(int[] arr)
+        {
+            for (int i = 1; i < arr.Length; i++)
+            {
+                int valueToSort = arr[i];
+                int j = i;
+
+                while (j > 0 && arr[j - 1] > valueToSort)
+                {
+                    arr[j] = arr[j - 1];
+                    j--;
+                }
+                arr[j] = valueToSort;
+            }
+        }
+
+        public static void PrintArray(int[] A)
+        {
+            for (int i = 0; i < A.Length; i++)
+            {
+                Console.Write(A[i] + " ");
+            }
+
+
+        }
+
+    
+
+        public static void populateArray(int[] A)
+        {
+            Random rnd = new Random();
+            for (int i = 0; i < A.Length; i++)
+            {
+                A[i] = rnd.Next(1000);
+            }
+        }
+    }
+}
---
+++ Sorting_Algorithms/SelectionSort/SelectionSort/Program.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SelectionSort
+{
+    class Program
+    {
+        public static Stopwatch stopwatch = Stopwatch.StartNew();
+        static void Main(string[] args)
+        {
+
+            Random r = new Random();
+            int[] integers = new int[10000];
+            Console.Write("Numbers before sorting  -->  ");
+            for (int i = 0; i < integers.Length; i++)
+            {
+                integers[i] = r.Next(1 , 101);
+
+                Console.Write(integers[i]  + " ");
+            }
+          
+            // Displaying the values generated automatically
+            
+
+            Console.WriteLine();
+
+            // Displaying the values genereted automatically sorted ascending
+            Console.Write("Numbers after sorting  -->   ");
+            initializeInsertionSort(integers);
+
+            for (int i = 0; i < integers.Length; i++)
+            {
+                Console.Write(integers[i] + " ");
+            }
+
+            Console.WriteLine();
+          
+            stopwatch.Stop();
+            Console.WriteLine("Running time : {0}s " ,stopwatch.Elapsed.TotalSeconds);
+            Console.ReadKey();
+
+        }
+
+
+
+        public static void initializeInsertionSort(int[] integers)
+        {
+            
+            for (int i = 1; i < integers.Length; i++)
+            {
+
+                int j = i;
+                int B = integers[i];
+
+                while ((j > 0) && (integers[j - 1] > B))
+                {
+                    integers[j] = integers[j - 1];
+                    j--;
+                }
+                integers[j] = B;
+            }
+
+        }
+    }
+}
+
+
+
